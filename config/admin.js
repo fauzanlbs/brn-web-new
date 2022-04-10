25dade145354c209859c59955b8afc2dbf9030d2
fauzan_lubis <f.lubis@indoleads.com>
2022-04-11 03:09:39 +0800

    update theme

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  url: '/',
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'db7dc695082ef2c18975a132f805e6e3'),
   },
--- src/admin/app.js
+++ src/admin/app.js
@@ -2,6 +2,7 @@ import AuthLogo from './extensions/brn-logo.png';
 import MenuLogo from './extensions/brn-logo.png';
 // import favicon from './extensions/favicon.ico';
 
+
 export default {
   config: {
     // Replace the Strapi logo in auth (login) views
@@ -21,12 +22,24 @@ export default {
     // Override or extend the theme
     theme: {
       colors: {
-        primary100: '#f6ecfc',
-        primary200: '#e0c1f4',
-        primary500: '#ac73e6',
-        primary600: '#9736e8',
-        primary700: '#8312d1',
-        danger700: '#b72b1a'
+        danger100: '#403949', // TOGGLE OFF BG
+        danger700: '#c13c58', // TOGGLE OFF COLOR
+        neutral0: '#241b2e', // BACKGROUND SIDEBAR
+        neutral100: '#262334', // BACKGROUND CONTENT
+        neutral150: '#241b2e', // DISABLED BUTTON BG
+        neutral200: '#262334', // LINE THROUGH MIDDLE
+        neutral500: '#8e8ea9', // MENU IDLE COLOR
+        neutral600: '#9a96a4', // MENU HOVER COLOR
+        neutral700: '#9a96a4', // MENU ACTIVE ICON COLOR
+        neutral800: '#afa6b6', // TITLE COLORS
+        neutral900: '#403949', // TOOLTIP BG
+        primary100: '#403949', // MENU ACTIVE BG
+        primary200: '#241b2e', // PLUS SIGN BG
+        primary500: '#7f7145', // PRIMARY BUTTON HOVER
+        primary600: '#bba551', // PRIMARY BUTTON COLOR
+        primary700: '#33adae', // HIGHLIGHT COLOR
+        secondary100: '#241b2e', // INFO BOX COLOR
+        success100: '#241b2e', // SUCCESS BOX BG
       },
     },
     translations: {
