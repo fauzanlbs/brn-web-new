24564ed6fa210a60da120416ea14c92673daa157
fauzan_lubis <f.lubis@indoleads.com>
2022-04-19 15:26:29 +0800

    update populate kategori

--- src/api/kategori/controllers/kategori.js
+++ src/api/kategori/controllers/kategori.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::kategori.kategori');
+// module.exports = createCoreController('api::kategori.kategori');
+
+module.exports = createCoreController("api::kategori.kategori", ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = { ...ctx.query, populate: '*' }
+
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    // some more custom logic
+
+    return { data, meta };
+  }
+}));
