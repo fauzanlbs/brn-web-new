2809fcc619c74c048435242c03a8e533825547f9
fauzan_lubis <f.lubis@indoleads.com>
2022-04-20 15:57:52 +0800

    update schema

---
+++ src/api/transaksi-detail/controllers/transaksi-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  transaksi-detail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::transaksi-detail.transaksi-detail');
---
+++ src/api/transaksi-detail/routes/transaksi-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * transaksi-detail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::transaksi-detail.transaksi-detail');
